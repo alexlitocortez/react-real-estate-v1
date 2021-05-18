0eb52baff54500196988c58476edd684413c2392
Alex Cortez <acim650@gmail.com>
2021-05-18 10:09:01 -0700

    src/components

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,18 +8,20 @@ export const Button = styled(Link)`
     outline: none;
     border: none;
     min-width: 100px;
-    max-width: 200px;
+    max-width: 400px;
     cursor: pointer;
     text-decoration: none;
     transition: 0.3s;
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: ${({big}) => (big ? '16px 40px' : '14px 24px')}
-    color: ${({ primary }) => (primary ? '#fff' : '#000d1a')}
-    font-size: ${({ big }) => (big ? '20px' : '14px')};
+    padding: ${({big}) => (big ? '30px 50px' : '14px 24px')};
+    color: #fff;
+    font-size: ${({ big }) => (big ? '40px' : '14px')};
 
     &:hover {
         transform: translateY(-2px);
     }
 `
+
+
